ec12a4ba7799720330e6998aedfd1f049a05e49b
Kjetil Golid <kjetil.golid@gmail.com>
2018-05-22 01:23:30 +0200

    tweak initial numbers in example

--- example.js
+++ example.js
@@ -4,23 +4,23 @@ import * as dat from './node_modules/dat.gui/build/dat.gui.module.js';
 
 window.onload = function() {
   var canvas = document.getElementById('main_canvas');
-  canvas.style.width = '1000px';
-  canvas.style.height = '1000px';
+  canvas.style.width = '100%';
+  canvas.style.height = '100%';
   if (canvas.getContext) {
     var ctx = canvas.getContext('2d');
     ctx.strokeStyle = '#1c2021';
 
     let options = {
-      radius: 15,
-      wh_ratio: 1,
+      radius: 14,
       symmetric: true,
-      roundness: 0.8,
-      solidness: 0.2,
-      compactness: 0.94,
-      block_size: 0.84,
+      roundness: 0.1,
+      solidness: 0.5,
+      compactness: 0.9,
+      block_size: 0.82,
       chance_vertical: 0.5,
-      rows: 4,
+      rows: 5,
       columns: 5,
+      padding: 40,
       color1: '#6c843e',
       color2: '#dc383a',
       color3: '#687d99',
@@ -38,8 +38,9 @@ window.onload = function() {
     let gui = new dat.GUI();
     gui.remember(options);
     let f1 = gui.addFolder('Layout');
-    f1.add(options, 'rows', 1, 7, 1).onFinishChange(run);
-    f1.add(options, 'columns', 1, 7, 1).onFinishChange(run);
+    f1.add(options, 'rows', 1, 12, 1).onFinishChange(run);
+    f1.add(options, 'columns', 1, 12, 1).onFinishChange(run);
+    f1.add(options, 'padding', 0, 120, 5).onFinishChange(run);
 
     let f2 = gui.addFolder('Apparatus Shape');
     f2.add(options, 'radius', 5, 30, 1).onFinishChange(run);
@@ -96,7 +97,7 @@ window.onload = function() {
     let apparat_size_x = apparatus.xdim * cell_size;
     let apparat_size_y = apparatus.ydim * cell_size;
 
-    let padding = 50;
+    let padding = options.padding - 100;
     let nx = options.columns;
     let ny = options.rows;
 
