76bfaf30861304a7bcf548bd342ab6ad744ac14f
Kjetil Golid <kjetil.golid@gmail.com>
2018-05-29 02:08:06 +0200

    add blueprint preset

--- presets.js
+++ presets.js
@@ -1,64 +1,4 @@
-/*
-
-let my_default = {
-  rows: 5,
-  columns: 5,
-  padding: 40,
-  cell_size: 10,
-  radius_x: 14,
-  radius_y: 14,
-  roundness: 0.1,
-  solidness: 0.5,
-  compactness: 0.9,
-  block_size: 0.82,
-  chance_vertical: 0.5,
-  h_symmetric: true,
-  v_symmetric: false,
-  display_stroke: true,
-  background_color: '#eeeee5',
-  color1: '#6c843e',
-  color2: '#dc383a',
-  color3: '#687d99',
-  color4: '#705f84',
-  color5: '#fc9a1a',
-  color6: '#aa3a33',
-  color7: '#9c4257',
-  color_mode: 'group',
-  group_size: 0.85
-};
-
-let mural = {
-  rows: 3,
-  columns: 3,
-  padding: 120,
-  cell_size: 5,
-  radius_x: 50,
-  radius_y: 50,
-  roundness: 1,
-  solidness: 1,
-  compactness: 1,
-  block_size: 0.5,
-  chance_vertical: 0.5,
-  h_symmetric: false,
-  v_symmetric: false,
-  display_stroke: false,
-  background_color: '#f6ddbb',
-  color1: '#d63644',
-  color2: '#396c68',
-  color3: '#f59647',
-  color4: '#4d3240',
-  color5: '#893f49',
-  color6: '#89e3b7',
-  color7: '#817c77',
-  color_mode: 'group',
-  group_size: 0.9
-};
-
-export { my_default, mural };
-*/
-
 export default {
-  preset: 'Default',
   remembered: {
     Default: {
       '0': {
@@ -114,6 +54,34 @@ export default {
         color7: '#817c77',
         color_mode: 'group',
         group_size: 0.9
+      }
+    },
+    blueprint: {
+      '0': {
+        rows: 1,
+        columns: 3,
+        padding: 210,
+        cell_size: 5,
+        radius_x: 31,
+        radius_y: 100,
+        roundness: 0,
+        solidness: 0.65,
+        compactness: 1,
+        block_size: 0.92,
+        chance_vertical: 0,
+        h_symmetric: true,
+        v_symmetric: false,
+        display_stroke: true,
+        background_color: '#dedede',
+        color1: '#dedede',
+        color2: '#dedede',
+        color3: '#dedede',
+        color4: '#dedede',
+        color5: '#dedede',
+        color6: '#dedede',
+        color7: '#dedede',
+        color_mode: 'group',
+        group_size: 0.76
       }
     }
   },
